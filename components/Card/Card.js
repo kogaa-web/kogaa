d14ccadeba261f9b303c09a7bbf807365859111b
vladislavbagnyuk <vladislav.bagnyuk@gmail.com>
2020-11-14 18:04:47 +0100

    Rendering whole card on image load

--- components/Card/Card.js
+++ components/Card/Card.js
@@ -1,35 +1,50 @@
 import Link from "next/link";
+import { useState, useCallback } from "react";
 
 import classes from "./Card.module.css";
 import { formatDate } from "../../lib/util";
 
 export default function card({ post }) {
+  const [loaded, setLoaded] = useState(false);
+  if (!post.featuredImage) {
+    setLoaded(true);
+  }
   let imageClass = classes.Circle;
   if (post.contentType.node.name === "architects") {
     imageClass = classes.Triangle;
   }
+
+  const onLoad = useCallback(() => {
+    console.log("loaded");
+    setLoaded(true);
+  }, []);
+
   return (
     <div className={classes.Card}>
-      {post.featuredImage ? (
-        <Link href={`/${post.slug}`}>
-          <a>
-            <img
-              className={imageClass}
-              src={post.featuredImage.node.sourceUrl}
-              alt={post.title}
-            />
-          </a>
-        </Link>
-      ) : null}
-      <p className={classes.Date}>{formatDate(post.date)}</p>
       <Link href={`/${post.slug}`}>
         <a>
-          <h2>{post.title}</h2>
+          <img
+            className={imageClass}
+            src={post.featuredImage.node.sourceUrl}
+            alt={post.title}
+            onLoad={onLoad}
+          />
         </a>
       </Link>
-      <p className={classes.Excerpt}>
-        {post.excerpt.replace(/(<([^>]+)>)/gi, "")}
-      </p>
+
+      {loaded ? (
+        <>
+          <p className={classes.Date}>{formatDate(post.date)}</p>
+          <Link href={`/${post.slug}`}>
+            <a>
+              <h2>{post.title}</h2>
+            </a>
+          </Link>
+          <p className={classes.Excerpt}>
+            {post.excerpt.replace(/(<([^>]+)>)/gi, "")}
+          </p>
+        </>
+      ) : null}
     </div>
   );
 }
