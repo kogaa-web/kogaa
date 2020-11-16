c4d22c4660343d1c22d3c123ceabe574eda86643
vladislavbagnyuk <vladislav.bagnyuk@gmail.com>
2020-11-16 11:36:59 +0100

    Links in footer

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -3,6 +3,10 @@ import classes from "./Footer.module.css";
 const footer = () => (
   <footer className={classes.Footer}>
     <hr />
+    <a href="#">Instagram</a>
+    <a href="#">Facebook</a>
+    <a href="#">Linkedin</a>
+    <a href="mailto:office@kogaa.eu">office@kogaa.eu</a>
     <p>© 2020 KOGAA Studio</p>
   </footer>
 );
