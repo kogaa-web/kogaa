1f6e74d8d8530e9df67ac69b0a71385db3bb1c4e
vladislavbagnyuk <vladislav.bagnyuk@gmail.com>
2020-11-16 11:24:17 +0100

    Basic menu layout

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -1,9 +1,11 @@
-import Footer from "../Footer/Footer";
 import classes from "./Layout.module.css";
+import Footer from "../Footer/Footer";
+import Menu from "../Menu/Menu";
 
 export default function layout(props) {
   return (
     <div className={classes.Layout}>
+      <Menu />
       {props.children}
       <Footer />
     </div>
---
+++ components/Menu/Menu.js
@@ -0,0 +1,16 @@
+import classes from "./Menu.module.css";
+import Logo from "../../assets/kogaa-logo.svg";
+import Circle from "../../assets/circle.svg";
+import Square from "../../assets/square.svg";
+import Triangle from "../../assets/triangle.svg";
+
+export default function menu() {
+  return (
+    <div className={classes.MainMenu}>
+      <Logo />
+      <Circle />
+      <Square />
+      <Triangle />
+    </div>
+  );
+}
