b5337101afc9589f9fb576a047501d7a4e691f19
vladislavbagnyuk <vladislav.bagnyuk@gmail.com>
2020-11-16 11:45:49 +0100

    Basic mobile footer layout

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -3,11 +3,14 @@ import classes from "./Footer.module.css";
 const footer = () => (
   <footer className={classes.Footer}>
     <hr />
-    <a href="#">Instagram</a>
-    <a href="#">Facebook</a>
-    <a href="#">Linkedin</a>
-    <a href="mailto:office@kogaa.eu">office@kogaa.eu</a>
-    <p>© 2020 KOGAA Studio</p>
+    <div className={classes.Links}>
+      <a href="#">Instagram</a>
+      <a href="#">Facebook</a>
+      <a href="#">Linkedin</a>
+      <a href="mailto:office@kogaa.eu">office@kogaa.eu</a>
+      <p className={classes.CopyrightMobile}>© 2020 KOGAA</p>
+    </div>
+    <p className={classes.CopyrightDesktop}>© 2020 KOGAA Studio</p>
   </footer>
 );
 
