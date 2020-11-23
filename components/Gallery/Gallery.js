860e9e6409888741d8bfca79b1db94898e048bf5
vladislavbagnyuk <vladislav.bagnyuk@gmail.com>
2020-11-23 23:13:21 +0100

    Basic gallery

--- components/Gallery/Gallery.js
+++ components/Gallery/Gallery.js
@@ -1,11 +1,35 @@
+import { useState } from "react";
 import classes from "./Gallery.module.css";
 
 export default function gallery({ images }) {
+  const [index, setIndex] = useState(0);
   return (
     <div className={classes.Gallery}>
-      {images.map((image) => {
-        return <img key={image.sourceUrl} src={image.sourceUrl} alt="" />;
-      })}
+      <div className={classes.Images}>
+        {images.map((image, number) => {
+          return (
+            <img
+              key={image.sourceUrl}
+              src={image.sourceUrl}
+              alt=""
+              className={
+                number === index ? classes.ActiveImage : classes.InactiveImage
+              }
+            />
+          );
+        })}
+      </div>
+      <div className={classes.Indicators}>
+        {images.map((image, number) => {
+          return (
+            <button
+              key={image.sourceUrl}
+              onClick={() => setIndex(number)}
+              className={number === index ? classes.Active : null}
+            />
+          );
+        })}
+      </div>
     </div>
   );
 }
