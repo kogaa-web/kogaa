63e789334ff1c39b034b157ad7ed7eebbe1d4ee8
vladislavbagnyuk <vladislav.bagnyuk@gmail.com>
2020-11-24 09:58:10 +0100

    Gallery scrolling

--- components/Gallery/Gallery.js
+++ components/Gallery/Gallery.js
@@ -3,6 +3,20 @@ import classes from "./Gallery.module.css";
 
 export default function gallery({ images }) {
   const [index, setIndex] = useState(0);
+
+  const nextImage = () => {
+    if (index !== images.length - 1) {
+      setIndex(index + 1);
+    }
+    console.log(index);
+  };
+  const previousImage = () => {
+    if (index !== 0) {
+      setIndex(index - 1);
+    }
+    console.log(index);
+  };
+
   return (
     <div className={classes.Gallery}>
       <div className={classes.Images}>
@@ -19,6 +33,9 @@ export default function gallery({ images }) {
           );
         })}
       </div>
+      <button className={classes.Previous} onClick={previousImage} />
+      <button className={classes.Fullscreen} />
+      <button className={classes.Next} onClick={nextImage} />
       <div className={classes.Indicators}>
         {images.map((image, number) => {
           return (
