3b87604c5c599444f47bd3c645efce02b1a4f0e8
vladislavbagnyuk <vladislav.bagnyuk@gmail.com>
2020-12-05 15:46:49 +0100

    Fixed mobile footer

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -2,12 +2,17 @@ import classes from "./Footer.module.css";
 
 const footer = () => (
   <footer className={classes.Footer}>
-    <div className={classes.Links}>
+    <div className={classes.Social}>
       <a href="#">Instagram</a>
       <a href="#">Facebook</a>
       <a href="#">Linkedin</a>
-      <a href="mailto:office@kogaa.eu">office@kogaa.eu</a>
-      <p className={classes.CopyrightMobile}>© 2020 KOGAA</p>
+      <a className={classes.EmailDesktop} href="mailto:office@kogaa.eu">
+        office@kogaa.eu
+      </a>
+      <div className={classes.CopyrightMobile}>
+        <a href="mailto:office@kogaa.eu">office@kogaa.eu</a>
+        <p>© 2020 KOGAA</p>
+      </div>
       <p className={classes.CopyrightDesktop}>© 2020 KOGAA Studio</p>
     </div>
   </footer>
