3cabd66d691ba3263a21daa200be5da9a97a6224
vladislavbagnyuk <vladislav.bagnyuk@gmail.com>
2021-01-21 09:22:56 +0100

    Working

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -1,3 +1,4 @@
+import { useState, useEffect } from "react";
 import Head from "next/head";
 
 import Footer from "../Footer/Footer";
@@ -5,50 +6,100 @@ import Menu from "../Menu/Menu";
 
 import classes from "./Layout.module.css";
 
-const layout = (props) => (
-  <div className={classes.Layout}>
-    <Head>
-      <link
-        rel="apple-touch-icon"
-        sizes="180x180"
-        href="/apple-touch-icon.png"
-      />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="32x32"
-        href="/favicon-32x32.png"
-      />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="16x16"
-        href="/favicon-16x16.png"
-      />
-      <link rel="manifest" href="/site.webmanifest" />
-      <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#EF4123" />
-      <meta name="msapplication-TileColor" content="#EF4123" />
-      <meta name="theme-color" content="#FFFFFF" />
-      <script
-        async
-        src="https://www.googletagmanager.com/gtag/js?id=G-LM24JH3WGJ"
-      ></script>
-      <script
-        dangerouslySetInnerHTML={{
-          __html: `
+const layout = (props) => {
+  const [prevScroll, setPrevScroll] = useState(0);
+  const [style, setStyle] = useState({
+    transition: "top 0.3s !important",
+    height: "auto",
+  });
+
+  // Adding and removing scroll handler
+  useEffect(() => {
+    window.addEventListener("scroll", onScrollHandler);
+    return () => window.removeEventListener("scroll", onScrollHandler);
+  });
+
+  // Detecting scroll to bottom of the page
+  const onScrollHandler = () => {
+    const scrollPosition = window.scrollY;
+    console.log(scrollPosition, prevScroll);
+    if (scrollPosition > 200) {
+      if (scrollPosition < prevScroll) {
+        setStyle({
+          zIndex: 10,
+          transition: "top 0.3s !important",
+          position: "fixed",
+          top: 0,
+          left: 0,
+          right: 0,
+          height: "auto",
+        });
+      } else {
+        setStyle({
+          zIndex: 10,
+          transition: "top 0.3s !important",
+          position: "fixed",
+          top: "-100%",
+          left: 0,
+          right: 0,
+          height: "auto",
+        });
+      }
+    } else {
+      setStyle({
+        transition: "top 0.3s !important",
+        height: "auto",
+      });
+    }
+    setPrevScroll(scrollPosition);
+  };
+  return (
+    <>
+      <Head>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#EF4123" />
+        <meta name="msapplication-TileColor" content="#EF4123" />
+        <meta name="theme-color" content="#FFFFFF" />
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-LM24JH3WGJ"
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
       window.dataLayer = window.dataLayer || []; function gtag()
       {dataLayer.push(arguments)}
       gtag("js", new Date()); gtag("config", "G-LM24JH3WGJ");
         `,
-        }}
-      />
-    </Head>
-    <div className={classes.Content}>
-      <Menu {...props} />
-      {props.children}
-    </div>
-    <Footer hideLine={props.hideLine} />
-  </div>
-);
+          }}
+        />
+      </Head>
+      <div className={classes.Layout} style={style}>
+        <Menu {...props} />
+      </div>
+      <div className={classes.Layout}>
+        <div className={classes.Content}>{props.children}</div>
+        <Footer hideLine={props.hideLine} />
+      </div>
+    </>
+  );
+};
 
 export default layout;
--- components/Menu/Menu.js
+++ components/Menu/Menu.js
@@ -26,8 +26,6 @@ const Menu = ({
 }) => {
   const [category, setCategory] = useState(currentCategory);
   const [loading, setLoading] = useState(false);
-  const [prevScroll, setPrevScroll] = useState(0);
-  const [style, setStyle] = useState({});
 
   const router = useRouter();
   if (router.events) {
@@ -47,42 +45,6 @@ const Menu = ({
       setCategory(postCategory);
     }
   }, [postCategory]);
-
-  // Adding and removing scroll handler
-  useEffect(() => {
-    window.addEventListener("scroll", onScrollHandler);
-    return () => window.removeEventListener("scroll", onScrollHandler);
-  });
-
-  // Detecting scroll to bottom of the page
-  const onScrollHandler = () => {
-    const scrollPosition = window.scrollY;
-    console.log(scrollPosition, prevScroll);
-    if (scrollPosition > 200) {
-      if (scrollPosition < prevScroll) {
-        setStyle({
-          zIndex: 10,
-          transition: "top 0.3s !important",
-          position: "fixed",
-          top: 0,
-          left: 0,
-          right: 0,
-        });
-      } else {
-        setStyle({
-          zIndex: 10,
-          transition: "top 0.3s !important",
-          position: "fixed",
-          top: "-100%",
-          left: 0,
-          right: 0,
-        });
-      }
-    } else {
-      setStyle({ transition: "top 0.3s !important" });
-    }
-    setPrevScroll(scrollPosition);
-  };
 
   let categoryIndex = null;
   switch (category) {
@@ -118,91 +80,83 @@ const Menu = ({
     }
   };
   return (
-    <div
-      className={
-        error
-          ? `${classes.MenuHeight} ${classes.ErrorHeight}`
-          : classes.MenuHeight
-      }
-    >
-      <div style={style}>
-        <motion.div className={classes.Menu} layoutId="menu" {...fadeIn}>
-          <div
-            onMouseLeave={() =>
-              loading ? null : setCategoryOnHover(currentCategory)
-            }
-          >
-            <div className={classes.MainMenu}>
-              <Link href="/">
-                <a>
-                  <Logo />
-                </a>
-              </Link>
-              <Link href="/news">
-                <a onMouseEnter={() => setCategoryOnHover("news")}>
-                  <Circle
-                    className={category === "news" ? classes.Active : null}
-                  />
-                </a>
-              </Link>
-              <Link href="/projects">
-                <a onMouseEnter={() => setCategoryOnHover("projects")}>
-                  <Square
-                    className={category === "projects" ? classes.Active : null}
-                  />
-                </a>
-              </Link>
-              <Link href="/about">
-                <a onMouseEnter={() => setCategoryOnHover("about")}>
-                  <Triangle
-                    className={category === "about" ? classes.Active : null}
-                  />
-                </a>
-              </Link>
-            </div>
-
-            {subcategories && category && !postSubcategories ? (
-              <Subcategories
-                category={category}
-                categories={subcategories[categoryIndex].nodes}
-                currentSubcategory={currentSubcategory}
-                onClick={() => setSelected(true)}
-              />
-            ) : null}
-
-            <motion.div style={{ background: "white" }} {...fadeIn}>
-              {postSubcategories && !currentCategory ? (
-                <motion.div className={classes.PostSubcategories} {...fadeIn}>
-                  <Back
-                    onClick={() => router.back()}
-                    style={{ cursor: "pointer" }}
-                  />
-                  {error ? (
-                    <Link href="/">
-                      <a>home</a>
-                    </Link>
-                  ) : (
-                    postSubcategories.map((category, index) => {
-                      return (
-                        <Fragment key={category.name}>
-                          <Link href={`/${postCategory}/${category.name}`}>
-                            <a>{category.name}</a>
-                          </Link>
-                          {postSubcategories[index + 1] && "|"}
-                        </Fragment>
-                      );
-                    })
-                  )}
-                </motion.div>
-              ) : null}
-              {date && !currentCategory ? (
-                <motion.div {...fadeIn} className={classes.Date}>
-                  {formatDate(date)}
-                </motion.div>
-              ) : null}
-            </motion.div>
+    <div>
+      <div className={classes.Menu}>
+        <div
+          onMouseLeave={() =>
+            loading ? null : setCategoryOnHover(currentCategory)
+          }
+        >
+          <div className={classes.MainMenu}>
+            <Link href="/">
+              <a>
+                <Logo />
+              </a>
+            </Link>
+            <Link href="/news">
+              <a onMouseEnter={() => setCategoryOnHover("news")}>
+                <Circle
+                  className={category === "news" ? classes.Active : null}
+                />
+              </a>
+            </Link>
+            <Link href="/projects">
+              <a onMouseEnter={() => setCategoryOnHover("projects")}>
+                <Square
+                  className={category === "projects" ? classes.Active : null}
+                />
+              </a>
+            </Link>
+            <Link href="/about">
+              <a onMouseEnter={() => setCategoryOnHover("about")}>
+                <Triangle
+                  className={category === "about" ? classes.Active : null}
+                />
+              </a>
+            </Link>
           </div>
-        </motion.div>
+
+          {subcategories && category && !postSubcategories ? (
+            <Subcategories
+              category={category}
+              categories={subcategories[categoryIndex].nodes}
+              currentSubcategory={currentSubcategory}
+              onClick={() => setSelected(true)}
+            />
+          ) : null}
+
+          <motion.div style={{ background: "white" }} {...fadeIn}>
+            {postSubcategories && !currentCategory ? (
+              <motion.div className={classes.PostSubcategories} {...fadeIn}>
+                <Back
+                  onClick={() => router.back()}
+                  style={{ cursor: "pointer" }}
+                />
+                {error ? (
+                  <Link href="/">
+                    <a>home</a>
+                  </Link>
+                ) : (
+                  postSubcategories.map((category, index) => {
+                    return (
+                      <Fragment key={category.name}>
+                        <Link href={`/${postCategory}/${category.name}`}>
+                          <a>{category.name}</a>
+                        </Link>
+                        {postSubcategories[index + 1] && "|"}
+                      </Fragment>
+                    );
+                  })
+                )}
+              </motion.div>
+            ) : null}
+            {date && !currentCategory ? (
+              <motion.div {...fadeIn} className={classes.Date}>
+                {formatDate(date)}
+              </motion.div>
+            ) : null}
+          </motion.div>
+        </div>
       </div>
     </div>
   );
