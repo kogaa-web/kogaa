dbec15655aa432807a428a2af9c8a9b86e04bb65
vladislavbagnyuk <vladislav.bagnyuk@gmail.com>
2021-02-01 17:57:49 +0100

    Deeplink page

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -7,42 +7,29 @@ import Line from "../../components/Line/Line";
 import classes from "./Footer.module.css";
 import { useEffect, useState } from "react";
 
-const footer = ({ hideLine }) => {
-  const [fbLink, setFbLink] = useState(
-    "https://www.facebook.com/kogaa.studio/"
-  );
-
-  useEffect(() => {
-    if (iOS()) {
-      setFbLink("fb://page/?id=822031704526911");
-    } else if (Android()) {
-      setFbLink("fb://page/822031704526911");
-    }
-  }, []);
-
-  return (
-    <motion.footer className={classes.Footer} {...fadeIn}>
-      {!hideLine ? <Line /> : null}
-      <div className={classes.Social}>
-        <a href="https://www.instagram.com/kogaa_studio" target="_blank">
-          Instagram
-        </a>
-        <a href={fbLink} target="_blank">
-          Facebook
-        </a>
-        <a href="https://www.linkedin.com/company/kogaa/" target="_blank">
-          Linkedin
-        </a>
-        <a className={classes.EmailDesktop} href="mailto:office@kogaa.eu">
-          office@kogaa.eu
-        </a>
-        <div className={classes.CopyrightMobile}>
-          <a href="mailto:office@kogaa.eu">office@kogaa.eu</a>
-          <p>© 2020 KOGAA</p>
-        </div>
-        <p className={classes.CopyrightDesktop}>© 2020 KOGAA studio</p>
+const footer = ({ hideLine }) => (
+  <motion.footer className={classes.Footer} {...fadeIn}>
+    {!hideLine ? <Line /> : null}
+    <div className={classes.Social}>
+      <a href="https://www.instagram.com/kogaa_studio" target="_blank">
+        Instagram
+      </a>
+      <a href="/facebook" target="_blank">
+        Facebook
+      </a>
+      <a href="https://www.linkedin.com/company/kogaa/" target="_blank">
+        Linkedin
+      </a>
+      <a className={classes.EmailDesktop} href="mailto:office@kogaa.eu">
+        office@kogaa.eu
+      </a>
+      <div className={classes.CopyrightMobile}>
+        <a href="mailto:office@kogaa.eu">office@kogaa.eu</a>
+        <p>© 2020 KOGAA</p>
       </div>
-    </motion.footer>
-  );
-};
+      <p className={classes.CopyrightDesktop}>© 2020 KOGAA studio</p>
+    </div>
+  </motion.footer>
+);
+
 export default footer;
---
+++ pages/facebook.js
@@ -0,0 +1,38 @@
+import { useEffect } from "react";
+
+import { iOS, Android } from "../lib/util";
+
+const Facebook = () => {
+  useEffect(() => {
+    if (process.browser) {
+      window.onload = () => {
+        const desktopLink = "https://www.facebook.com/kogaa.studio/";
+        const androidLink = "fb://page/822031704526911";
+        const iosLink = "fb://page/?id=822031704526911";
+
+        if (iOS()) {
+          window.location = iosLink;
+        } else if (Android()) {
+          window.location = androidLink;
+          window.setTimeout(() => {
+            window.location = desktopLink;
+          }, 25);
+        } else {
+          window.location = desktopLink;
+          window.setTimeout(() => {
+            window.location = desktopLink;
+          }, 25);
+        }
+
+        const killPopup = () => {
+          window.removeEventListener("pagehide", killPopup);
+        };
+
+        window.addEventListener("pagehide", killPopup);
+      };
+    }
+  }, []);
+  return null;
+};
+
+export default Facebook;
