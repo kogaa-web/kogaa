02b101737e4a42ab83fa14aefe7e02ac90beda59
Vladislav Bagnyuk <vladislav.bagnyuk@gmail.com>
2023-11-27 11:54:02 +0100

    fix

--- components/GalleryFullscreen/Gallery.js
+++ components/GalleryFullscreen/Gallery.js
@@ -5,7 +5,7 @@ import { motion, useAnimation } from "framer-motion";
 
 import classes from "./Gallery.module.css";
 
-export default function gallery({ images }) {
+export default function Gallery({ images }) {
   const animationSpeed = 0.4;
   const [fullscreen, setFullscreen] = useState(false);
   const [index, setIndex] = useState(0);
