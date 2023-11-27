3489b4b85d8b630204e623eb9685a9619f24910c
Vladislav Bagnyuk <vladislav.bagnyuk@gmail.com>
2023-11-27 12:30:16 +0100

    manual scroll restoration

--- containers/Category/Category.js
+++ containers/Category/Category.js
@@ -79,7 +79,36 @@ const Category = ({
   useEffect(() => {
     window.addEventListener("scroll", onScrollHandler);
     return () => window.removeEventListener("scroll", onScrollHandler);
-  });
+  }, []);
+
+  // set scroll restoration to manual
+  useEffect(() => {
+    if (
+      "scrollRestoration" in history &&
+      history.scrollRestoration !== "manual"
+    ) {
+      history.scrollRestoration = "manual";
+    }
+  }, []);
+
+  // handle and store scroll position
+  useEffect(() => {
+    const handleRouteChange = () => {
+      sessionStorage.setItem("scrollPosition", window.scrollY.toString());
+    };
+    router.events.on("routeChangeStart", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeStart", handleRouteChange);
+    };
+  }, [router.events]);
+
+  // restore scroll position
+  useEffect(() => {
+    if ("scrollPosition" in sessionStorage) {
+      window.scrollTo(0, Number(sessionStorage.getItem("scrollPosition")));
+      sessionStorage.removeItem("scrollPosition");
+    }
+  }, []);
 
   // Detecting scroll to bottom of the page
   const onScrollHandler = () => {
